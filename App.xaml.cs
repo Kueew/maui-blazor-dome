7b791c95dae4aebd86d5b95bb729853c800aade4
Joe <admin@kuew.net>
2023-01-15 13:31:51 +0800

    init

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
